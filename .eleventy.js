9cbd0458db32e324be29629cc35a3e57d41af686
Glitch (first-end) <none>
2020-10-06 20:19:26 +0000

    📇⛩ Checkpoint ./.eleventy.js:636128/492 ./src/_includes/layouts/default.njk:636128/361 ./package.json:636128/124

--- .eleventy.js
+++ .eleventy.js
@@ -5,8 +5,17 @@ const pluginSass = require("eleventy-plugin-sass");
 module.exports = function(eleventyConfig) {
   
   // eleventyConfig.addFilter( "myFilter", function() {});
+    
+  eleventyConfig.addPlugin(pluginSass, {
+    watch: ['src/assets/css/*.scss', '!node_modules/**'],
+  });
   
-  eleventyConfig.addPlugin(pluginSass,{watch: ['src/assets/css/*.scss', '!node_modules/**']});
+//   return {
+//     dir: { input: 'site', output: 'public', data: '_data' },
+//     passthroughFileCopy: true,
+//     templateFormats: ['njk', 'md', 'css', 'html', 'yml',"txt"],
+//     htmlTemplateEngine: 'njk'
+//   }
  
   return {
     dir: {
