3910fb88ba7a39af3e298253807e5c18837630f3
Glitch (first-end) <none>
2020-10-06 19:33:42 +0000

    👻🙇 Checkpoint ./package.json:636128/1654 ./.eleventy.js:636128/525 ./src/assets/css/styles.scss:636128/42

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,12 @@
 // docs: https://www.11ty.io/docs/config/
 
+const pluginSass = require("eleventy-plugin-sass");
+
 module.exports = function(eleventyConfig) {
   
   // eleventyConfig.addFilter( "myFilter", function() {});
+  
+  eleventyConfig.addPlugin(pluginSass,{watch: ['src/assets/css/*.scss', '!node_modules/**']});
  
   return {
     dir: {
